99dca3d0fd3e40f1e1baf6ae7e3aa0cdb2031783
ishikado <okazakisaburo@gmail.com>
2019-10-17 00:04:32 +0900

    いくつかの unwrap を expect に置き換え

--- src/hatagenpei/controller.rs
+++ src/hatagenpei/controller.rs
@@ -333,18 +333,17 @@ impl ScoreOperation for ScoresInRedis {
     }
 }
 
-// TODO 実装する
 impl ScoreOperation for ScoresInPostgre {
     fn get_progress(&mut self, player_name: &str) -> Progress {
         // postgre に接続
-        let conn = Connection::connect(&self.postgre_uri[..], TlsMode::None).unwrap();
+        let conn = Connection::connect(&self.postgre_uri[..], TlsMode::None).expect("failed to connect postgres");
 
         // 見つからなかった場合は、insert を実行する
         let select_query = format!(
             "SELECT name, data FROM {} where name = $1",
             DB_HATAGENPEI_PROGRESS_KEY
         );
-        let res = conn.query(&select_query[..], &[&player_name]).unwrap();
+        let res = conn.query(&select_query[..], &[&player_name]).expect("failed to select query for get_progress");
 
         if res.len() == 0 {
             let progress = Progress::new(
@@ -384,7 +383,7 @@ impl ScoreOperation for ScoresInPostgre {
 
     fn insert_progress(&mut self, progress: &Progress) -> bool {
         // postgre に接続
-        let conn = Connection::connect(&self.postgre_uri[..], TlsMode::None).unwrap();
+        let conn = Connection::connect(&self.postgre_uri[..], TlsMode::None).expect("failed to connect postgres");
 
         // すでに要素が存在している場合は、SQL update
         // そうでない場合は SQL insert を行う
@@ -394,7 +393,7 @@ impl ScoreOperation for ScoresInPostgre {
         );
         let res = conn
             .query(&select_query[..], &[&&progress.user.name[..]])
-            .unwrap();
+            .expect("failed to select query for insert_progress");
 
         let jsonstr = serde_json::to_string(&progress).unwrap();
         if res.len() == 0 {
@@ -404,7 +403,7 @@ impl ScoreOperation for ScoresInPostgre {
                 DB_HATAGENPEI_PROGRESS_KEY
             );
             conn.execute(&insert_query[..], &[&&progress.user.name[..], &jsonstr])
-                .unwrap();
+                .expect("failed to insert query for insert_progress");
         } else {
             // update
             let update_query = format!(
@@ -412,21 +411,23 @@ impl ScoreOperation for ScoresInPostgre {
                 DB_HATAGENPEI_PROGRESS_KEY
             );
             conn.execute(&update_query[..], &[&jsonstr, &&progress.user.name[..]])
-                .unwrap();
+                .expect("failed to update query for insert_progress");
         }
         return true;
     }
 
     fn delete_progress(&mut self, player_name: &str) -> bool {
         // postgre に接続
-        let conn = Connection::connect(&self.postgre_uri[..], TlsMode::None).unwrap();
+        let conn = Connection::connect(&self.postgre_uri[..], TlsMode::None).expect("failed to connect postgres");
         let delete_query = format!("DELETE FROM {} where name = $1", DB_HATAGENPEI_PROGRESS_KEY);
-        conn.execute(&delete_query[..], &[&player_name]).unwrap();
+        conn.execute(&delete_query[..], &[&player_name])
+            .expect("failed to delete query for delete_progress");
+
         return true;
     }
     fn update_winloses(&mut self, player_name: &str, is_player_win: bool) -> bool {
         // postgre に接続
-        let conn = Connection::connect(&self.postgre_uri[..], TlsMode::None).unwrap();
+        let conn = Connection::connect(&self.postgre_uri[..], TlsMode::None).expect("failed to connect postgres");
 
         // 勝敗を取得
         let select_query = format!(
@@ -475,7 +476,7 @@ impl ScoreOperation for ScoresInPostgre {
 
     fn get_win_loses(&self) -> Vec<WinLose> {
         let mut res = vec![];
-        let conn = Connection::connect(&self.postgre_uri[..], TlsMode::None).unwrap();
+        let conn = Connection::connect(&self.postgre_uri[..], TlsMode::None).expect("failed to connect postgres");
 
         // 勝敗を取得
         let select_query = format!(
